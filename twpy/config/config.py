5905a3cc10bc3cc27ae76205f6c85fa66d07d738
Fardin Allahverdinazhand <0x0ptim0us@gmail.com>
2020-02-25 17:33:40 +0330

    mobile user-agent added

--- twpy/config/config.py
+++ twpy/config/config.py
@@ -1,6 +1,7 @@
-VERSION = "1.2.3"
+VERSION = "1.2.4"
 BASE_URL = "https://twitter.com/"
 MOBILE_URL = "https://mobile.twitter.com/"
+APIV1_URL = "https://help.twitter.com/api/v1/username_lookups?username="
 TIMELINE_WITH_TOKEN_QUERY = "i/search/timeline?vertical=default&src=unkn&include_available_features=1&include_entities=1" \
                  "&max_position=%TOKEN%&reset_error_state=false&f=tweets&q="
 
